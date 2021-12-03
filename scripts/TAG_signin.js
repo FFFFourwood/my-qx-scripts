612b71bcf763b7c51ecce8edef302cb46c0bc209
FFFFourwood <34063465+FFFFourwood@users.noreply.github.com>
2021-12-03 14:43:52 +0800

    Update TAG_signin.js

--- scripts/TAG_signin.js
+++ scripts/TAG_signin.js
@@ -12,7 +12,8 @@ const method = "POST";
 const headers = {
     "cookie": cookie,
     "referer": "https://taggood-4.xyz/user",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+    "Content-Type": "application/json;charset=UTF-8"
 };
 
 function unicodeConverter(str) {
@@ -25,8 +26,8 @@ const myRequest = {
 };
 $task.fetch(myRequest).then(response => {
     // response.statusCode, response.headers, response.body
-    let data = JSON.parse(JSON.stringify(response.body))
-    console.log(typeof(data));
+    let data = response.body
+    console.log(typeof(data) + ';;;;' + data);
     $notify("TAG签到提醒", "", data.msg); // Success!
     $done();
 }, reason => {
